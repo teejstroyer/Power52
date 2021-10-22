ef83128642ec49f065c406df08e43af0cc6d7d78
teejstroyer <titoriano@gmail.com>
2021-10-21 23:53:40 -0500

    created gameboard and base for dragging

--- power_52/lib/main.dart
+++ power_52/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -24,26 +26,46 @@ class GameBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Array Length-1 minus index will give you inversed index.
-
-    return Container(
-      color: Colors.black,
-      child: AspectRatio(
-          aspectRatio: 1 / 1,
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              crossAxisSpacing: 0,
-              mainAxisSpacing: 0,
-            ),
-            itemCount: 16,
-            itemBuilder: (_, index) => Container(
-              decoration: BoxDecoration(
-                color: Colors.yellow,
-                border: Border.all(color: Colors.black, width: 3),
+    var screenSize = MediaQuery.of(context).size;
+    var size = ([screenSize.height, screenSize.width].reduce(min) / 4) - 15;
+    return Column(
+      children: [
+        Expanded(child: Container(color: Colors.blue)),
+        Container(
+          color: Colors.black,
+          child: AspectRatio(
+            aspectRatio: 1 / 1,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                crossAxisSpacing: 0,
+                mainAxisSpacing: 0,
               ),
-              child: Center(child: Text(index.toString())),
+              itemCount: 16,
+              itemBuilder: (_, index) => DragTarget(
+                builder: (BuildContext context, List<Object?> candidateData, List<dynamic> rejectedData) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      border: Border.all(color: Colors.black, width: 3),
+                    ),
+                    child: Center(child: Text(index.toString())),
+                  );
+                },
+              ),
             ),
-          )),
+          ),
+        ),
+        Expanded(
+          child: Row(children: [
+            Draggable(
+              child: Container(color: Colors.pink, width: size / 2, height: size),
+              feedback: Container(color: Colors.pink, width: size / 2, height: size),
+              childWhenDragging: const SizedBox.shrink(),
+            ),
+          ]),
+        ),
+      ],
     );
   }
 }
--- power_52/macos/Flutter/GeneratedPluginRegistrant.swift
+++ power_52/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
 import flutter_webrtc
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
